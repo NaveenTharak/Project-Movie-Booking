6ef75f0d53f2c453777f379c049afe2e4447d780
Amaya Kumarasiri <amayakumarasiri7@gmail.com>
2024-01-08 17:19:06 +0530

    Admin Dev

--- Froutend/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/kavindulakshitha/Desktop/Movie App/frontend/src/app/layout\":[]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
